6c47e2d92a131495009e5f310781f986fca2469b
baltzi_j <jean-francois.baltzinger@epitech.eu>
2012-02-12 17:46:37 +0100

    4.2 fix hodir quests

---
+++ sql/updates/world/reanemu/2012_01_06_100_world_hodir_spawn_lok'lira.sql
@@ -0,0 +1,5 @@
+-- Spawn Lok'lira (29975), asignacion de guid mediante auto_increment
+
+INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES
+
+(29975, 571, 1, 1, 0, 0, 6917.26, -1541.88, 833.81, 1.11376, 600, 0, 0, 12175, 0, 0, 0, 0, 0);

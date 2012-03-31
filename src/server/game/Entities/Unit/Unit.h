5aa35a77a0e0dfabd26a8ad5613a3dbafff5efd6
baltzi_j <jean-francois.baltzinger@epitech.eu>
2012-03-31 14:33:33 +0200

    add new anticheat system

--- src/server/game/Entities/Unit/Unit.h
+++ src/server/game/Entities/Unit/Unit.h
@@ -2221,6 +2221,7 @@ class Unit : public WorldObject
     protected:
         explicit Unit (bool isWorldObject);
 
+        GameObject* m_temp_transport;
         UnitAI* i_AI, *i_disabledAI;
 
         void _UpdateSpells(uint32 time);

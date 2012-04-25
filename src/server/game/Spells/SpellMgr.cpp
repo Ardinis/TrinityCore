82a75fecf048a4a93a59e46549ea700db2901da8
Gabii <bastiangabriel@hotmail.com>
2012-04-25 14:14:06 +0200

    Fix Stack of Ebon Plague

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3226,7 +3226,7 @@ void SpellMgr::LoadDbcDataCorrections()
             case 51735: // Ebon Plague
             case 51734:
             case 51726:
-                spellInfo->AttributesEx3 |= SPELL_ATTR3_STACK_FOR_DIFF_CASTERS;
+                //spellInfo->AttributesEx3 |= SPELL_ATTR3_STACK_FOR_DIFF_CASTERS;
                 spellInfo->SpellFamilyFlags[2] = 0x10;
                 spellInfo->EffectApplyAuraName[1] = SPELL_AURA_MOD_DAMAGE_PERCENT_TAKEN;
                 break;

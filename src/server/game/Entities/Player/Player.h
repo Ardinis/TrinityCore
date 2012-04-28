c9d0275904a7bd7f051cfa0e6256f6cde8ceb061
baltzi_j <jean-francois.baltzinger@epitech.eu>
2012-04-28 13:22:13 +0200

    Revert "Revert "Revert "reimplement d'un anticheat passif"""
    This reverts commit 63692df88b756e15e49aebac9ff1b942ba11abce.

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1087,8 +1087,6 @@ class Player : public Unit, public GridObject<Player>
         explicit Player (WorldSession* session);
         ~Player ();
 
-        //AnticheatData anticheatData;
-
         void CleanupsBeforeDelete(bool finalCleanup = true);
 
         static UpdateMask updateVisualBits;

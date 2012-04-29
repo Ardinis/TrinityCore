da55d612721818fceb59bdf5b1e42b4c62f41630
baltzi_j <jean-francois.baltzinger@epitech.eu>
2012-04-29 20:40:35 +0200

    Adding database declaration in Main.cpp of worlserver

--- src/server/worldserver/Main.cpp
+++ src/server/worldserver/Main.cpp
@@ -52,6 +52,9 @@ int m_ServiceStatus = -1;
 WorldDatabaseWorkerPool WorldDatabase;                      ///< Accessor to the world database
 CharacterDatabaseWorkerPool CharacterDatabase;              ///< Accessor to the character database
 LoginDatabaseWorkerPool LoginDatabase;                      ///< Accessor to the realm/login database
+WebDatabaseWorkerPool WebDatabase;                      ///< Accessor to the realm/login database
+StoreDatabaseWorkerPool StoreDatabase;                      ///< Accessor to the realm/login database
+
 
 uint32 realmID;                                             ///< Id of the realm
 

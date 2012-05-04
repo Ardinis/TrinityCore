e909c470b46f1008921913f7b356818bc3ae9617
Benjy <benjy@paragon-servers.com>
2012-05-04 10:45:52 +0200

    fix putricide summons

--- src/server/scripts/Northrend/IcecrownCitadel/boss_professor_putricide.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_professor_putricide.cpp
@@ -189,7 +189,7 @@ class boss_professor_putricide : public CreatureScript
                 if (!(events.GetPhaseMask() & PHASE_MASK_NOT_SELF))
                     instance->SetBossState(DATA_PROFESSOR_PUTRICIDE, NOT_STARTED);
                 instance->SetData(DATA_NAUSEA_ACHIEVEMENT, uint32(true));
-
+		start = false;
                 events.Reset();
                 summons.DespawnAll();
                 SetPhase(PHASE_COMBAT_1);
@@ -312,6 +312,7 @@ class boss_professor_putricide : public CreatureScript
 
             void DamageTaken(Unit* /*attacker*/, uint32& /*damage*/)
             {
+	      start = true;
                 switch (_phase)
                 {
                     case PHASE_COMBAT_1:
@@ -555,20 +556,19 @@ class boss_professor_putricide : public CreatureScript
                 if (me->HasUnitState(UNIT_STATE_CASTING))
                     return;
 
-                while (uint32 eventId = events.ExecuteEvent())
-                {
-
-		  if (_phase != PHASE_COMBAT_3)
+		  if (_phase != PHASE_COMBAT_3 && start)
 		    {
 		      if (uiUnstableExperiment <= diff)
 			{
-			  std::cout << "unstable expermiment gogogo" << std::endl;
 			  Talk(EMOTE_UNSTABLE_EXPERIMENT);
 			  DoCast(me, SPELL_UNSTABLE_EXPERIMENT);
 			  uiUnstableExperiment = urand(35000, 40000);
 			}
 		      else uiUnstableExperiment -= diff;
 		    }
+
+                while (uint32 eventId = events.ExecuteEvent())
+                {
 
                     switch (eventId)
                     {
@@ -719,6 +719,7 @@ class boss_professor_putricide : public CreatureScript
             uint8 _oozeFloodStage;
             bool _experimentState;
 	  uint64 uiUnstableExperiment;
+	  bool start;
         };
 
         CreatureAI* GetAI(Creature* creature) const

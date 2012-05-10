4a900538621098cea21d1170c07e7eb92edf909b
Gabii <bastiangabriel@hotmail.com>
2012-05-10 08:27:09 +0200

    Reduce Time for Item loose with AH

--- src/server/game/Mails/Mail.cpp
+++ src/server/game/Mails/Mail.cpp
@@ -194,7 +194,7 @@ void MailDraft::SendMailTo(SQLTransaction& trans, MailReceiver const& receiver, 
         if (m_COD)
             expire_delay = 3 * DAY;
         else
-            expire_delay = pSender && pSender->isGameMaster() ? 90 * DAY : 30 * DAY;
+            expire_delay = pSender && pSender->isGameMaster() ? 21 * DAY : 7 * DAY;
 
     time_t expire_time = deliver_time + expire_delay;
 

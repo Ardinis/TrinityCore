2c1d1dad2c4fc0c03aa0fd5cb938b2a90454047d
baltzi_j <jean-francois.baltzinger@epitech.eu>
2012-07-22 16:11:30 +0200

    ulduar : teleporteur

--- src/server/scripts/Northrend/Ulduar/Ulduar/ulduar_teleporter.cpp
+++ src/server/scripts/Northrend/Ulduar/Ulduar/ulduar_teleporter.cpp
@@ -107,10 +107,10 @@ class ulduar_teleporter : public GameObjectScript
             player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Expedition Base Camp", GOSSIP_SENDER_MAIN, BASE_CAMP);
             if (InstanceScript* instance = gameObject->GetInstanceScript())
             {
-                if (instance->GetData(DATA_COLOSSUS) == 2) //count of 2 collossus death
-                    player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Formation Grounds", GOSSIP_SENDER_MAIN, GROUNDS);
-                if (instance->GetBossState(BOSS_LEVIATHAN) == DONE)
-                    player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Colossal Forge", GOSSIP_SENDER_MAIN, FORGE);
+	      //                if (instance->GetData(DATA_COLOSSUS) == 2) //count of 2 collossus death
+	      //                    player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Formation Grounds", GOSSIP_SENDER_MAIN, GROUNDS);
+	      //       if (instance->GetBossState(BOSS_LEVIATHAN) == DONE)
+	      player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Colossal Forge", GOSSIP_SENDER_MAIN, FORGE);
                 if (instance->GetBossState(BOSS_XT002) == DONE)
                 {
                     player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Scrapyard", GOSSIP_SENDER_MAIN, SCRAPYARD);

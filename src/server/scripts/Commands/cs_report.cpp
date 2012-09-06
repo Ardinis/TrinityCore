334989de73f3fb7f54eb95e8938f908ef5f504c6
baltzi_j <jean-francois.baltzinger@epitech.eu>
2012-09-06 02:07:59 +0200

    add missing files :: it's not my day with this

---
+++ src/server/scripts/Commands/cs_report.cpp
@@ -0,0 +1,266 @@
+#include "ScriptMgr.h"
+#include "ObjectMgr.h"
+#include "Common.h"
+#include "DatabaseEnv.h"
+#include "World.h"
+#include "Player.h"
+#include "Opcodes.h"
+#include "Chat.h"
+#include "ObjectAccessor.h"
+#include "Language.h"
+#include "AccountMgr.h"
+#include "SystemConfig.h"
+#include "revision.h"
+#include "Util.h"
+#include "math.h"
+
+class report_commandscript : public CommandScript
+{
+public:
+    report_commandscript() : CommandScript("report_commandscript") { }
+
+    ChatCommand* GetCommands() const
+    {
+        static ChatCommand reportCommandTable[] =
+        {
+            { "quest",            SEC_ADMINISTRATOR,  false, &HandleReportQuest,                    "", NULL },
+            { "complete",       SEC_ADMINISTRATOR,  false, &HandleReportComplete,               "", NULL },
+            { "remove",         SEC_ADMINISTRATOR,  false, &HandleReportRemove,                 "", NULL },
+            { NULL,             0,                  false, NULL,                               "", NULL }
+        };
+        static ChatCommand commandTable[] =
+        {
+            { "report",          SEC_ADMINISTRATOR,  false, NULL,                  "", reportCommandTable },
+            { NULL,             0,                  false, NULL,                               "", NULL }
+        };
+        return commandTable;
+    }
+
+    static bool HandleReportQuest(ChatHandler* handler, const char* args)
+    {
+      Player *player = handler->GetSession()->GetPlayer();
+      int32 statut = 0;
+      int32 count = 1;
+      bool alreadyReportByPlayer = true;
+      if (!args)
+	return false;
+      char* qId = handler->extractKeyFromLink((char *)(args), "Hquest");
+      if (!qId)
+	return false;
+      if (qId[0] >= '0' || qId[0] >= '9')
+	if (uint32 quest = atoi(qId))
+	  {
+	    //  uint32 quest = extractQuestIdFromLink((char*)args);
+	    if (!quest)
+	      return false;
+	    QueryResult result = WorldDatabase.PQuery("SELECT status, count FROM report_quest WHERE id='%d'", quest);
+	    if (!result)
+	      {
+		WorldDatabase.PExecute("INSERT INTO report_quest(id, count, comment) VALUES('%d', '%d', '%s')", quest, count, "");
+		CharacterDatabase.PExecute("INSERT INTO char_report_quests(guid, quest) VALUES('%d', '%d')", player->GetGUID(), quest);
+		handler->PSendSysMessage(LANG_REPORT_QUEST_FIRST_REPORT);
+		return true;
+	      }
+	    Field *fields = result->Fetch();
+	    statut = fields[0].GetInt32();
+	    count = fields[1].GetInt32();
+	    QueryResult resultit = CharacterDatabase.PQuery("SELECT guid FROM char_report_quests WHERE quest='%d' and guid = '%d'", quest, player->GetGUID());
+	    if (!resultit)
+	      alreadyReportByPlayer = false;
+	    else
+	      {
+		//		handler->PSendSysMessage(LANG_REPORT_QUEST_ALREADY_REPORT);
+		alreadyReportByPlayer = true;
+	      }
+	    switch(statut)
+	      {
+	      case 0:
+		handler->PSendSysMessage(LANG_REPORT_QUEST_STATUT_UNVERIFIED);
+		break;
+	      case 1:
+		{
+		  uint32 entry = quest;
+		  Quest const* pQuest = sObjectMgr->GetQuestTemplate(entry);
+
+		  if (!pQuest || player->GetQuestStatus(entry) == QUEST_STATUS_NONE)
+		    {
+		      handler->PSendSysMessage(LANG_REPORT_QUEST_NOT_FOUND);
+		      return false;
+		    }
+
+		  // Add quest items for quests that require items
+		  for (uint8 x = 0; x < QUEST_ITEM_OBJECTIVES_COUNT; ++x)
+		    {
+		      uint32 id = pQuest->RequiredItemId[x];
+		      uint32 count = pQuest->RequiredItemCount[x];
+		      if (!id || !count)
+			continue;
+
+		      uint32 curItemCount = player->GetItemCount(id, true);
+
+		      ItemPosCountVec dest;
+		      uint8 msg = player->CanStoreNewItem(NULL_BAG, NULL_SLOT, dest, id, count-curItemCount);
+		      if (msg == EQUIP_ERR_OK)
+			{
+			  Item* item = player->StoreNewItem(dest, id, true);
+			  player->SendNewItem(item, count-curItemCount, true, false);
+			}
+		    }
+
+		  // All creature/GO slain/casted (not required, but otherwise it will display "Creature slain 0/10")
+		  for (uint8 i = 0; i < QUEST_OBJECTIVES_COUNT; ++i)
+		    {
+		      int32 creature = pQuest->RequiredNpcOrGo[i];
+		      uint32 creaturecount = pQuest->RequiredNpcOrGoCount[i];
+
+		      if (uint32 spell_id = pQuest->RequiredSpellCast[i])
+			{
+			  for (uint16 z = 0; z < creaturecount; ++z)
+			    player->CastedCreatureOrGO(creature, 0, spell_id);
+			}
+		      else if (creature > 0)
+			{
+			  if (CreatureTemplate const* cInfo = sObjectMgr->GetCreatureTemplate(creature))
+			    for (uint16 z = 0; z < creaturecount; ++z)
+			      player->KilledMonster(cInfo, 0);
+			}
+		      else if (creature < 0)
+			{
+			  for (uint16 z = 0; z < creaturecount; ++z)
+			    player->CastedCreatureOrGO(creature, 0, 0);
+			}
+		    }
+
+		  // If the quest requires reputation to complete
+		  if (uint32 repFaction = pQuest->GetRepObjectiveFaction())
+		    {
+		      uint32 repValue = pQuest->GetRepObjectiveValue();
+		      uint32 curRep = player->GetReputationMgr().GetReputation(repFaction);
+		      if (curRep < repValue)
+			if (FactionEntry const* factionEntry = sFactionStore.LookupEntry(repFaction))
+			  player->GetReputationMgr().SetReputation(factionEntry, repValue);
+		    }
+
+		  // If the quest requires a SECOND reputation to complete
+		  if (uint32 repFaction = pQuest->GetRepObjectiveFaction2())
+		    {
+		      uint32 repValue2 = pQuest->GetRepObjectiveValue2();
+		      uint32 curRep = player->GetReputationMgr().GetReputation(repFaction);
+		      if (curRep < repValue2)
+			if (FactionEntry const* factionEntry = sFactionStore.LookupEntry(repFaction))
+			  player->GetReputationMgr().SetReputation(factionEntry, repValue2);
+		    }
+
+		  // If the quest requires money
+		  int32 ReqOrRewMoney = pQuest->GetRewOrReqMoney();
+		  if (ReqOrRewMoney < 0)
+		    player->ModifyMoney(-ReqOrRewMoney);
+
+		  player->CompleteQuest(entry);
+		  handler->PSendSysMessage(LANG_REPORT_QUEST_STATUT_BUG);
+		  break;
+		}
+	      case 2:
+		handler->PSendSysMessage(LANG_REPORT_QUEST_STATUT_NOT_BUG);
+		break;
+	      default :
+		break;
+	      }
+	    if (!alreadyReportByPlayer)
+	      {
+		count++;
+		CharacterDatabase.PExecute("INSERT INTO char_report_quests(guid, quest) VALUES('%d', '%d')", player->GetGUID(), quest);
+		WorldDatabase.PExecute("UPDATE report_quest SET count = '%d' where id='%d'"	, count, quest);
+	      }
+	    return true;
+	  }
+      return false;
+    }
+
+    static bool HandleReportRemove(ChatHandler* handler, const char* args)
+    {
+      Player *player = handler->GetSession()->GetPlayer();
+      int32 statut = 0;
+
+
+      if (!args)
+	return false;
+      char* qId = handler->extractKeyFromLink((char *)(args), "Hquest");
+      if (!qId)
+	return false;
+
+      // id case (explicit or from shift link)
+      if (qId[0] >= '0' || qId[0] >= '9')
+	if (uint32 quest = atoi(qId))
+	  {
+	    //  uint32 quest = extractQuestIdFromLink((char*)args);
+	    if (!quest)
+	      return false;
+
+	    QueryResult result = WorldDatabase.PQuery("SELECT status, count FROM report_quest WHERE id='%d'", quest);
+
+	    if (!result)
+	      return false;
+
+	    Field *fields = result->Fetch();
+	    statut = fields[0].GetInt32();
+
+	    //Is the quest is bug ?
+	    if (statut != 2)
+	      {
+	      WorldDatabase.PQuery("UPDATE report_quest SET status = 2 where id='%d'", quest);
+		handler->PSendSysMessage(LANG_REPORT_QUEST_REMOVE_SUCCESS);
+	      }
+	    else
+		handler->PSendSysMessage(LANG_REPORT_QUEST_REMOVE_FAIL);
+	    return true;
+	  }
+      return false;
+    }
+
+    static bool HandleReportComplete(ChatHandler* handler, const char* args)
+    {
+      Player *player = handler->GetSession()->GetPlayer();
+      int32 statut = 0;
+
+      if (!args)
+	return false;
+      char* qId = handler->extractKeyFromLink((char *)(args), "Hquest");
+      if (!qId)
+	return false;
+
+      // id case (explicit or from shift link)
+      if (qId[0] >= '0' || qId[0] >= '9')
+	if (uint32 quest = atoi(qId))
+	  {
+	    //  uint32 quest = extractQuestIdFromLink((char*)args);
+	    if (!quest)
+	      return false;
+
+	    QueryResult result = WorldDatabase.PQuery("SELECT status, count FROM report_quest WHERE id='%d'", quest);
+
+	    if (!result)
+	      return false;
+
+	    Field *fields = result->Fetch();
+	    statut = fields[0].GetInt32();
+
+	    if (statut != 1)
+	      {
+		handler->PSendSysMessage(LANG_REPORT_QUEST_COMPLETE_SUCCESS);
+		//	handler->PSendSysMessage(LANG_REPORT_QUEST_WAITING);
+		WorldDatabase.PQuery("UPDATE report_quest SET status = 1 where id='%d'", quest);
+	      }
+	    else
+		handler->PSendSysMessage(LANG_REPORT_QUEST_COMPLETE_FAIL);
+	    //  handler->PSendSysMessage(LANG_REPORT_QUEST_SUCCESS);
+	    return true;
+	  }
+      return false;
+    }
+};
+
+void AddSC_report_commandscript()
+{
+    new report_commandscript();
+}

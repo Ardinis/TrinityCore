42609ce2f00cae7adb0efaeab30b92b91b349426
baltzi_j <jean-francois.baltzinger@epitech.eu>
2012-09-08 04:20:27 +0200

    Movement Handler part 1 This reverts commit ac29cf9cfecfc564f0315cb494310bc96270bb3b.

--- src/server/game/Handlers/QuestHandler.cpp
+++ src/server/game/Handlers/QuestHandler.cpp
@@ -98,7 +98,7 @@ void WorldSession::HandleQuestgiverHelloOpcode(WorldPacket & recv_data)
     if (GetPlayer()->HasUnitState(UNIT_STATE_DIED))
         GetPlayer()->RemoveAurasByType(SPELL_AURA_FEIGN_DEATH);
     // Stop the npc if moving
-    creature->GetMotionMaster()->StopMovement(25000);
+    creature->StopMoving();
 
     if (sScriptMgr->OnGossipHello(_player, creature))
         return;

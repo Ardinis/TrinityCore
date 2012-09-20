b5d2dfe130abc4df0c34e9cbde56a4641e4dd80b
baltzi_j <jean-francois.baltzinger@epitech.eu>
2012-09-20 05:23:54 +0200

    ICC : fix rotface mutated infection on MT1

--- src/server/scripts/Northrend/IcecrownCitadel/boss_rotface.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_rotface.cpp
@@ -248,6 +248,8 @@ class boss_rotface : public CreatureScript
 			    Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 1, 0.0f, true, -MUTATED_INFECTION);
 			    if (!target)
 			      target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true, -MUTATED_INFECTION);
+			    if (target == me->getVictim())
+			      target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0.0f, true, -MUTATED_INFECTION);
 			    if (target)
 			      me->CastCustomSpell(SPELL_MUTATED_INFECTION, SPELLVALUE_MAX_TARGETS, 1, target, false);
 			    events.ScheduleEvent(EVENT_MUTATED_INFECTION, 14000);

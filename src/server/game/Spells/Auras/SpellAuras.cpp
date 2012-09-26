67e3c6f968d06c498821f92d6500a7453c8938c8
baltzi_j <jean-francois.baltzinger@epitech.eu>
2012-09-26 04:17:28 +0200

    [Pretre] Extase , bug majeur

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1492,8 +1492,8 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
                         // check cooldown
                         if (caster->GetTypeId() == TYPEID_PLAYER)
                         {
-                            if (caster->ToPlayer()->HasSpellCooldown(aura->GetId()))
-                                break;
+			  if (caster->ToPlayer()->HasSpellCooldown(aura->GetId()) <= 10)
+			    break;
                             // and add if needed
                             caster->ToPlayer()->AddSpellCooldown(aura->GetId(), 0, uint32(time(NULL) + 12));
                         }

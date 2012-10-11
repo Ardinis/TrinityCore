b9477bef60413c83f086888f26cf4cfaeea0a40c
Tisk <jean-francois.baltzinger@epitech.eu>
2012-10-11 20:22:44 +0200

    Spell : priest Extase error in previous fix

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1492,9 +1492,8 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
                         // check cooldown
                         if (caster->GetTypeId() == TYPEID_PLAYER)
                         {
-			  if (caster->ToPlayer()->HasSpellCooldown(aura->GetId()))
-			    if (caster->ToPlayer()->HasSpellCooldown(aura->GetId()) <= 10)
-			      break;
+			  if (caster->ToPlayer()->GetSpellCooldownDelay(aura->GetId()) <= 5)
+			    break;
                             // and add if needed
                             caster->ToPlayer()->AddSpellCooldown(aura->GetId(), 0, uint32(time(NULL) + 12));
                         }

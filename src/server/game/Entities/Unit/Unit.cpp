97a6e0880a3f65b4af2fcb5efa252e4bccfa2dd8
Tisk <jean-francois.baltzinger@epitech.eu>
2012-10-13 23:08:02 +0200

    fix Beacon of Light

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -6762,7 +6762,8 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
 
 	      if (triggered_spell_id && beaconTarget)
 	      {
-		victim->CastCustomSpell(beaconTarget, triggered_spell_id, &basepoints0, NULL, NULL, true, 0, triggeredByAura);
+		if (caster)
+		  caster->CastCustomSpell(beaconTarget, triggered_spell_id, &basepoints0, NULL, NULL, true, 0, triggeredByAura);
 		return true;
 	      }
 

63fbb09c3f143c8e16c6a85cc84f5f8c75fc2e0a
Tisk <jean-francois.baltzinger@epitech.eu>
2013-05-24 09:15:48 +0200

    crash fix

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -3279,8 +3279,8 @@ void Spell::cast(bool skipCheck)
       // As of 3.0.2 pets begin attacking their owner's target immediately
       // Let any pets know we've attacked something. Check DmgClass for harmful spells only
       // This prevents spells such as Hunter's Mark from triggering pet attack
-      if (this->GetSpellInfo())
-	if (this->GetSpellInfo()->DmgClass != SPELL_DAMAGE_CLASS_NONE)
+      if (GetSpellInfo())
+	if (GetSpellInfo()->DmgClass != SPELL_DAMAGE_CLASS_NONE)
 	  if (Pet* playerPet = playerCaster->GetPet())
 	    if (playerPet->isAlive() && playerPet->isControlled() && (m_targets.GetTargetMask() & TARGET_FLAG_UNIT) &&  playerPet->AI() && m_targets.GetObjectTarget())
 	      playerPet->AI()->OwnerAttacked(m_targets.GetObjectTarget()->ToUnit());
@@ -3472,10 +3472,10 @@ void Spell::cast(bool skipCheck)
 
     if (Pet* pet = m_caster->ToPet())
     {
-        if (pet->GetCharmInfo() && pet->IsAIEnabled)
+      if (m_targets.GetUnitTarget() && pet->GetCharmInfo() && pet->IsAIEnabled)
         {
             // If pet is passive or was sent to cast a spell on a friendly target.
-            if ((pet->HasReactState(REACT_PASSIVE) || m_caster->IsFriendlyTo(unitTarget)) && pet->GetCharmInfo()->IsMovingForCast())
+	  if ((pet->HasReactState(REACT_PASSIVE) || m_caster->IsFriendlyTo(m_targets.GetUnitTarget())) && pet->GetCharmInfo()->IsMovingForCast())
             {
                 CharmInfo* charmInfo = pet->GetCharmInfo();
                 pet->AttackStop();

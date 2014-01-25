ecd3e0e03de6bd0867f9e70cdd31f04bef440ce3
Tisk <jean-francois.baltzinger@epitech.eu>
2014-01-25 17:40:52 +0100

    fix nefarian pre-bc script.

--- src/server/scripts/EasternKingdoms/BlackwingLair/boss_nefarian.cpp
+++ src/server/scripts/EasternKingdoms/BlackwingLair/boss_nefarian.cpp
@@ -235,7 +235,521 @@ public:
     };
 };
 
+class ClassCheck
+{
+public:
+    ClassCheck(Classes cl): _classes(cl) { }
+
+    bool operator() (Unit *target)
+    {
+        if (target->getClass() != _classes)
+            return true;
+
+        return false;
+    }
+
+private :
+    Classes _classes;
+};
+
+
+//    SPELL_MAGE                  = 23410,     //wild magic
+enum mageSpells
+{
+    SPELL_FIREBALL = 23411,
+    SPELL_GIVRE = 23412,
+    SPELL_ARCANE_BOOM = 23413,
+};
+
+class spell_nefarian_controled_mage : public SpellScriptLoader
+{
+public:
+    spell_nefarian_controled_mage() : SpellScriptLoader("spell_nefarian_controled_mage") { }
+
+    class spell_nefarian_controled_mage_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_nefarian_controled_mage_SpellScript);
+
+        bool Load()
+        {
+            return true;
+        }
+
+        void FilterTargets(std::list<Unit*>& unitList)
+        {
+            unitList.remove_if(ClassCheck(CLASS_MAGE));
+        }
+
+        void Register()
+        {
+            OnUnitTargetSelect += SpellUnitTargetFn(spell_nefarian_controled_mage_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENEMY);
+        }
+    };
+
+    class spell_nefarian_controled_mage_AuraScript : public AuraScript
+    {
+        PrepareAuraScript(spell_nefarian_controled_mage_AuraScript);
+
+        bool Validate(SpellInfo const* /*entry*/)
+        {
+            return true;
+        }
+
+        void HandleEffectPeriodic(AuraEffect const* aurEff)
+        {
+            if (Unit *owner = GetTarget())
+            {
+                switch (rand() % 3)
+                {
+                    case 0:
+                        owner->CastSpell(owner, SPELL_FIREBALL, true);
+                        break;
+                    case 1:
+                        owner->CastSpell(owner, SPELL_ARCANE_BOOM, true);
+                        break;
+                    case 2:
+                        owner->CastSpell(owner, SPELL_GIVRE, true);
+                        break;
+                    default:
+                        owner->CastSpell(owner, SPELL_FIREBALL, true);
+                        break;
+                        break;
+                }
+            }
+        }
+
+        void Register()
+        {
+            OnEffectPeriodic += AuraEffectPeriodicFn(spell_nefarian_controled_mage_AuraScript::HandleEffectPeriodic, EFFECT_0, SPELL_AURA_PERIODIC_TRIGGER_SPELL);
+        }
+    };
+
+    AuraScript* GetAuraScript() const
+    {
+        return new spell_nefarian_controled_mage_AuraScript();
+    }
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_nefarian_controled_mage_SpellScript();
+    }
+};
+
+//    SPELL_WARRIOR               = 23397,     //beserk
+class spell_nefarian_controled_warrior : public SpellScriptLoader
+{
+public:
+    spell_nefarian_controled_warrior() : SpellScriptLoader("spell_nefarian_controled_warrior") { }
+
+    class spell_nefarian_controled_warrior_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_nefarian_controled_warrior_SpellScript);
+
+        bool Load()
+        {
+            return true;
+        }
+
+        void FilterTargets(std::list<Unit*>& unitList)
+        {
+            unitList.remove_if(ClassCheck(CLASS_WARRIOR));
+        }
+
+        void Register()
+        {
+            OnUnitTargetSelect += SpellUnitTargetFn(spell_nefarian_controled_warrior_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENEMY);
+            OnUnitTargetSelect += SpellUnitTargetFn(spell_nefarian_controled_warrior_SpellScript::FilterTargets, EFFECT_1, TARGET_UNIT_SRC_AREA_ENEMY);
+        }
+    };
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_nefarian_controled_warrior_SpellScript();
+    }
+};
+
+//    SPELL_DRUID                 = 23398,     // cat form
+class spell_nefarian_controled_druid : public SpellScriptLoader
+{
+public:
+    spell_nefarian_controled_druid() : SpellScriptLoader("spell_nefarian_controled_druid") { }
+
+    class spell_nefarian_controled_druid_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_nefarian_controled_druid_SpellScript);
+
+        bool Load()
+        {
+            return true;
+        }
+
+        void FilterTargets(std::list<Unit*>& unitList)
+        {
+            unitList.remove_if(ClassCheck(CLASS_DRUID));
+        }
+
+        void Register()
+        {
+            OnUnitTargetSelect += SpellUnitTargetFn(spell_nefarian_controled_druid_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENEMY);
+            OnUnitTargetSelect += SpellUnitTargetFn(spell_nefarian_controled_druid_SpellScript::FilterTargets, EFFECT_1, TARGET_UNIT_SRC_AREA_ENEMY);
+        }
+    };
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_nefarian_controled_druid_SpellScript();
+    }
+};
+
+//    SPELL_PRIEST                = 23401,     // corrupted healing
+class spell_nefarian_controled_priest : public SpellScriptLoader
+{
+public:
+    spell_nefarian_controled_priest() : SpellScriptLoader("spell_nefarian_controled_priest") { }
+
+    class spell_nefarian_controled_priest_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_nefarian_controled_priest_SpellScript);
+
+        bool Load()
+        {
+            return true;
+        }
+
+        void FilterTargets(std::list<Unit*>& unitList)
+        {
+            unitList.remove_if(ClassCheck(CLASS_PRIEST));
+        }
+
+        void Register()
+        {
+            OnUnitTargetSelect += SpellUnitTargetFn(spell_nefarian_controled_priest_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENEMY);
+        }
+    };
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_nefarian_controled_priest_SpellScript();
+    }
+};
+
+/*
+DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=13 AND `SourceEntry` = 23415;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,`SourceId`,`ElseGroup`,`ConditionTypeOrReference`,`ConditionTarget`,`ConditionValue1`,`ConditionValue2`,`ConditionValue3`,`NegativeCondition`,`ScriptName`,`Comment`) VALUES
+(13,0,23415,0,0,18,0,1,11583,0,0,'', '');
+*/
+//    SPELL_PALADIN               = 23418,     //syphon blessing
+class spell_nefarian_controled_paladin : public SpellScriptLoader
+{
+public:
+    spell_nefarian_controled_paladin() : SpellScriptLoader("spell_nefarian_controled_paladin") { }
+
+    class spell_nefarian_controled_paladin_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_nefarian_controled_paladin_SpellScript);
+
+        bool Load()
+        {
+            return true;
+        }
+
+        void FilterTargets(std::list<Unit*>& unitList)
+        {
+            unitList.remove_if(ClassCheck(CLASS_PALADIN));
+        }
+
+        void Register()
+        {
+            OnUnitTargetSelect += SpellUnitTargetFn(spell_nefarian_controled_paladin_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENEMY);
+        }
+    };
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_nefarian_controled_paladin_SpellScript();
+    }
+};
+
+//    SPELL_SHAMAN                = 23425,     //totems
+enum totemSpells
+{
+    SPELL_TOTEM_1 = 23419,
+    SPELL_TOTEM_2 = 23420,
+    SPELL_TOTEM_3 = 23422,
+    SPELL_TOTEM_4 = 23423,
+};
+class spell_nefarian_controled_shaman : public SpellScriptLoader
+{
+public:
+    spell_nefarian_controled_shaman() : SpellScriptLoader("spell_nefarian_controled_shaman") { }
+
+    class spell_nefarian_controled_shaman_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_nefarian_controled_shaman_SpellScript);
+
+        bool Load()
+        {
+            return true;
+        }
+
+        void FilterTargets(std::list<Unit*>& unitList)
+        {
+            unitList.remove_if(ClassCheck(CLASS_SHAMAN));
+        }
+
+        void Register()
+        {
+            OnUnitTargetSelect += SpellUnitTargetFn(spell_nefarian_controled_shaman_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENEMY);
+        }
+    };
+
+    class spell_nefarian_controled_shaman_AuraScript : public AuraScript
+    {
+        PrepareAuraScript(spell_nefarian_controled_shaman_AuraScript);
+
+        bool Validate(SpellInfo const* /*entry*/)
+        {
+            return true;
+        }
+
+        void HandleEffectPeriodic(AuraEffect const* aurEff)
+        {
+            if (Unit *owner = GetTarget())
+            {
+                switch (rand() % 4)
+                {
+                    case 0:
+                        owner->CastSpell(owner, SPELL_TOTEM_1, true);
+                        break;
+                    case 1:
+                        owner->CastSpell(owner, SPELL_TOTEM_2, true);
+                        break;
+                    case 2:
+                        owner->CastSpell(owner, SPELL_TOTEM_3, true);
+                        break;
+                    case 3:
+                        owner->CastSpell(owner, SPELL_TOTEM_4, true);
+                        break;
+                    default:
+                        owner->CastSpell(owner, SPELL_TOTEM_1, true);
+                        break;
+                }
+            }
+        }
+        void Register()
+        {
+            OnEffectPeriodic += AuraEffectPeriodicFn(spell_nefarian_controled_shaman_AuraScript::HandleEffectPeriodic, EFFECT_0, SPELL_AURA_PERIODIC_TRIGGER_SPELL);
+        }
+    };
+
+    AuraScript* GetAuraScript() const
+    {
+        return new spell_nefarian_controled_shaman_AuraScript();
+    }
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_nefarian_controled_shaman_SpellScript();
+    }
+};
+
+//    SPELL_WARLOCK               = 23427,     //infernals
+class spell_nefarian_controled_warlock : public SpellScriptLoader
+{
+public:
+    spell_nefarian_controled_warlock() : SpellScriptLoader("spell_nefarian_controled_warlock") { }
+
+    class spell_nefarian_controled_warlock_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_nefarian_controled_warlock_SpellScript);
+
+        bool Load()
+        {
+            return true;
+        }
+
+        void FilterTargets(std::list<Unit*>& unitList)
+        {
+            unitList.remove_if(ClassCheck(CLASS_WARLOCK));
+        }
+
+        void Register()
+        {
+            OnUnitTargetSelect += SpellUnitTargetFn(spell_nefarian_controled_warlock_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENEMY);
+        }
+    };
+
+
+    class spell_nefarian_controled_warlock_AuraScript : public AuraScript
+    {
+        PrepareAuraScript(spell_nefarian_controled_warlock_AuraScript);
+
+        void HandleEffectApply(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
+        {
+            if (Unit* target = GetTarget())
+                target->CastSpell(target, 23426, true);
+        }
+
+        void Register()
+        {
+            OnEffectApply += AuraEffectApplyFn(spell_nefarian_controled_warlock_AuraScript::HandleEffectApply, EFFECT_0, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
+        }
+
+    };
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_nefarian_controled_warlock_SpellScript();
+    }
+
+    AuraScript* GetAuraScript() const
+    {
+        return new spell_nefarian_controled_warlock_AuraScript();
+    }
+
+};
+
+// SPELL_INFRNAL_SUMMON = 23426
+
+
+//    SPELL_HUNTER                = 23436,     //bow broke
+class spell_nefarian_controled_hunter : public SpellScriptLoader
+{
+public:
+    spell_nefarian_controled_hunter() : SpellScriptLoader("spell_nefarian_controled_hunter") { }
+
+    class spell_nefarian_controled_hunter_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_nefarian_controled_hunter_SpellScript);
+
+        bool Load()
+        {
+            return true;
+        }
+
+        void FilterTargets(std::list<Unit*>& unitList)
+        {
+            unitList.remove_if(ClassCheck(CLASS_HUNTER));
+        }
+
+        void Register()
+        {
+            OnUnitTargetSelect += SpellUnitTargetFn(spell_nefarian_controled_hunter_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENEMY);
+        }
+    };
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_nefarian_controled_hunter_SpellScript();
+    }
+};
+
+//    SPELL_ROGUE                 = 23414     //Paralise
+class spell_nefarian_controled_rogue : public SpellScriptLoader
+{
+public:
+    spell_nefarian_controled_rogue() : SpellScriptLoader("spell_nefarian_controled_rogue") { }
+
+    class spell_nefarian_controled_rogue_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_nefarian_controled_rogue_SpellScript);
+
+        bool Load()
+        {
+            return true;
+        }
+
+        void FilterTargets(std::list<Unit*>& unitList)
+        {
+            unitList.remove_if(ClassCheck(CLASS_ROGUE));
+        }
+
+        void Register()
+        {
+            OnUnitTargetSelect += SpellUnitTargetFn(spell_nefarian_controled_rogue_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_SRC_AREA_ENEMY);
+        }
+    };
+
+    SpellScript* GetSpellScript() const
+    {
+        return new spell_nefarian_controled_rogue_SpellScript();
+    }
+};
+
+class npc_nefarian_totem : public CreatureScript
+{
+public:
+    npc_nefarian_totem() : CreatureScript("npc_nefarian_totem") { }
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_nefarian_totemAI (creature);
+    }
+
+    enum totemEntrys
+    {
+        TOTEM_CORRUPTED_WINDFURY = 14666,
+        TOTEM_CORRUPTED_FIRENOVA = 14662,
+        TOTEM_CORRUPTED_STONESKIN = 14663,
+        TOTEM_CORRUPTED_HEAL = 14664,
+    };
+
+    struct npc_nefarian_totemAI : public Scripted_NoMovementAI
+    {
+        npc_nefarian_totemAI(Creature* creature) : Scripted_NoMovementAI(creature)
+        {
+            _despawn = false;
+        }
+
+        void DamageTaken(Unit* /*done_by*/, uint32 &damage)
+        {
+            damage = 0;
+            if (_despawn)
+                return;
+            _despawn = true;
+            me->DespawnOrUnsummon(500);
+        }
+
+        void Reset()
+        {
+            switch(me->GetEntry())
+            {
+                case TOTEM_CORRUPTED_WINDFURY:
+                    me->CastSpell(me, 32911, true);
+                    break;
+                case TOTEM_CORRUPTED_FIRENOVA:
+                    me->CastSpell(me, 30941, true);
+                    break;
+                case TOTEM_CORRUPTED_STONESKIN:
+                    me->CastSpell(me, 68893, true);
+                    break;
+                case TOTEM_CORRUPTED_HEAL:
+                    me->CastSpell(me, 52042,true);
+                    break;
+            }
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+        }
+
+    private:
+        bool _despawn;
+    };
+
+};
+
 void AddSC_boss_nefarian()
 {
     new boss_nefarian();
+    new spell_nefarian_controled_mage();
+    new spell_nefarian_controled_warrior();
+    new spell_nefarian_controled_druid();
+    new spell_nefarian_controled_priest();
+    new spell_nefarian_controled_warlock();
+    new spell_nefarian_controled_paladin();
+    new spell_nefarian_controled_hunter();
+    new spell_nefarian_controled_rogue();
+    new spell_nefarian_controled_shaman();
+    new npc_nefarian_totem();
 }

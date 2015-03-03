c7ba8556eb3071003950146e466b1627eb08c222
zangdar <klemm@7un.net>
2015-03-03 12:15:27 +0800

    SQL: Fix quest item usebug

---
+++ sql/Patchs dev sql/non appliqués/BA_Objet_Quete.sql
@@ -0,0 +1,3 @@
+DELETE FROM conditions WHERE SourceEntry IN (56448,36314);
+INSERT INTO conditions (SourceTypeOrReferenceId,SourceEntry,ConditionTarget, ConditionTypeOrReference,ConditionValue1,ConditionValue2, Comment) VALUES(17, 56448, 1, 31, 3, 30142, "Anti BA Storm Hammer");
+INSERT INTO conditions (SourceTypeOrReferenceId,SourceEntry,ConditionTarget, ConditionTypeOrReference,ConditionValue1,ConditionValue2, Comment) VALUES(17, 36314, 1, 31, 3, 20132, "Anti BA Presence voyant");
